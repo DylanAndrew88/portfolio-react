f1fefade5af5318e69e40cf84d021597e4215b83
DylanAndrew88 <Dandrewblake88@gmail.com>
2018-07-12 20:50:42 -0400

    changed routes

--- src/components/main.js
+++ src/components/main.js
@@ -4,17 +4,15 @@
 
   import LandingPage from './landingpage';
   import Contact from './contact';
-  import AboutMe from './aboutme';
   import Projects from './projects';
   import Resume from './resume';
 
 
   const Main = () => (
     <Switch>
-      <Route path="/home" component={LandingPage} />
+      <Route path="/main" component={LandingPage} />
       <Route path="/resume" component={Resume} />
       <Route path="/contact" component={Contact} />
-      <Route path="/aboutme" component={AboutMe} />
       <Route path="/projects" component={Projects} />
     </Switch>
   )
