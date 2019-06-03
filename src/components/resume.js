3d0b8bfa3df62183845ee7bf891f787733acc31c
dylan stump <dylanstump88@gmail.com>
2019-06-03 18:54:51 -0400

    updated resume

--- src/components/resume.js
+++ src/components/resume.js
@@ -5,7 +5,7 @@
     render() {
       return(
         <div className="resume-div">
-          <iframe src="https://www.docdroid.net/rbg0rzh/dylan-stump-developer-resume.pdf&embedded=true" style={{width:'950px', height:'670px', marginTop:'30px', marginBottom:'35px'}}></iframe>
+          <iframe src="https://www.docdroid.net/3lGkE0C/dylan-communications-resume.pdf" style={{width:'950px', height:'670px', marginTop:'30px', marginBottom:'35px'}}></iframe>
         </div>
       )
     }
